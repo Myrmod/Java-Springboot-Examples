12428cdf83a1a2aa9ce91bb16997c356f6e9fdb3
Pascal Galle <myrmod@gmx.de>
2024-12-03 12:39:54 +0100

    initialize project

---
+++ Certification/README.md
@@ -0,0 +1,1 @@
+https://spring.academy/courses/building-a-rest-api-with-spring-boot/lessons/introduction
---
+++ Certification/cashcard/src/test/java/de/myrmod/cashcard/CashcardApplicationTests.java
@@ -0,0 +1,13 @@
+package de.myrmod.cashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
