5d7f7b663edce40802953b3fcc4cc499d98ecf7f
Pascal Galle <myrmod@gmx.de>
2025-01-27 10:13:47 +0100

    remove security from public directory in FormAuthSecurityConfig

--- security/src/main/java/de/myrmod/security/Config/FormAuthSecurityConfig.java
+++ security/src/main/java/de/myrmod/security/Config/FormAuthSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -22,8 +23,6 @@ public class FormAuthSecurityConfig {
 		 */
 		httpSecurity
 			.authorizeHttpRequests(auth -> auth
-				.requestMatchers(HttpMethod.GET, "/public/**")
-				.permitAll()
 				.anyRequest()
 				.authenticated()
 			)
@@ -38,5 +37,10 @@ public class FormAuthSecurityConfig {
 			.logout(LogoutConfigurer::permitAll);
 
 		return httpSecurity.build();
+	}
+
+	@Bean
+	public WebSecurityCustomizer removeSecurityFromPublicDirectory() {
+		return (web) -> web.ignoring().requestMatchers("/public/**");
 	}
 }
